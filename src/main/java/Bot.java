78ec483cb9dc13b48d3c210c7dcb0219ba53362c
jumpifgreater <jumpifgreater@gmail.com>
2025-07-26 17:00:45 +0100

    alot still missing ,vc claim

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.internal.handle.ApplicationCommandPermissionsUpdateHandle
 
 public class Bot {
     public static void main(String[] args) {
-        JDABuilder.createDefault("MTM5ODM3NTc5MDc0MzM5MjI2Ng.G6yfhr.RZyV5CqKX03eknEmRwrJRW9OesB99kUHKykRT0", GatewayIntent.GUILD_MESSAGES,
+        JDABuilder.createDefault("token", GatewayIntent.GUILD_MESSAGES,
                 GatewayIntent.MESSAGE_CONTENT,
                 GatewayIntent.GUILD_MEMBERS,
                 GatewayIntent.GUILD_VOICE_STATES).addEventListeners(new VCManager(), new CommandHelper()).build();
